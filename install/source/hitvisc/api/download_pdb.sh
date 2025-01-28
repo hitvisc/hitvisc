363f77f3d6595e1c45bd88a446a9f0aa8129ff5d
hitvisc <nnikitina.krc@gmail.com>
2025-01-28 17:53:05 +0300

    Update download_pdb.sh - revert to initial commit

--- install/source/hitvisc/api/download_pdb.sh
+++ install/source/hitvisc/api/download_pdb.sh
@@ -29,55 +29,14 @@ TMPDIR="$1"
 PDBSTR="$2"
 TARGET_SYSTEM_NAME="$3"
 RESULT="NULL"
-COMMENT="NULL"
 
 if [[ "$PDBSTR" =~ ^[0-9A-Za-z]{4}$ ]]; then
-  PDBFILENAME="target_${TARGET_SYSTEM_NAME}.pdb" # filename in HiTViSc system
-  PDBURL="https://files.rcsb.org/download/${PDBSTR}.pdb"
-  status_code_pdb=$(curl -Is "$PDBURL" | head -n 1 | awk '{print $2}')
-  if [[ "$status_code_pdb" == "200" ]]; then
-    # Legacy PDB format file exists and is accessible: download it
-    wget -q -O "$TMPDIR/$PDBFILENAME" "$PDBURL" 2>/dev/null
-    if [ $? -ne 0 ]; then
-      rm "$TMPDIR/$PDBFILENAME"; RESULT="NULL";
-      log_msg_error "Failed to download PDB file ($PDBURL)"
-      return $CODEOTHERERR
-    else
-      RESULT="$TMPDIR/$PDBFILENAME"
-      COMMENT="Note: legacy PDB format file for PDB ID $PDBSTR has been retrieved from $PDBURL."
-    fi
-  else
-    # Legacy PDB format file not found: check the availability of the PDB bundle in the PDB FTP archive
-    DIR=$(cut -c 2-3 <<<"$PDBSTR" | tr '[:upper:]' '[:lower:]')
-    PDBSTRlower=$(echo "$PDBSTR" | tr '[:upper:]' '[:lower:]')
-    BUNDLEFILENAME="${PDBSTRlower}-pdb-bundle.tar.gz"
-    BUNDLEURL=$(echo "https://files.wwpdb.org/pub/pdb/compatible/pdb_bundle/$DIR/$PDBSTRlower/$BUNDLEFILENAME")
-    status_code_bundle=$(curl -Is "$BUNDLEURL" | head -n 1 | awk '{print $2}')
-      if [[ "$status_code_bundle" == "200" ]]; then
-        wget -q -O "$TMPDIR/$BUNDLEFILENAME" "$BUNDLEURL" 2>/dev/null
-        if [ $? -ne 0 ]; then
-          rm "$TMPDIR/$BUNDLEFILENAME"; RESULT="NULL";
-          log_msg_error "Failed to download PDB bundle ($BUNDLEURL)"
-          return $CODEOTHERERR
-        else
-          ORIGPDBFILENAME="${PDBSTRlower}-pdb-bundle1.pdb"
-          if tar --list -f "$TMPDIR/$BUNDLEFILENAME" | grep -q "$ORIGPDBFILENAME"; then
-            if tar -xzvf "$TMPDIR/$BUNDLEFILENAME" --to-stdout "$ORIGPDBFILENAME" > "$TMPDIR/$PDBFILENAME"; then
-              RESULT="$TMPDIR/$PDBFILENAME"
-              COMMENT="Note: legacy PDB format file for PDB ID $PDBSTR was not available for this structure. The best effort/minimal PDB format file has been retrieved from $BUNDLEURL (file $ORIGPDBFILENAME). See more information at https://www.rcsb.org/docs/general-help/structures-without-legacy-pdb-format-files."
-              echo "$COMMENT"
-            else
-              log_msg_error "Error extracting PDB bundle ($ORIGPDBFILENAME from $TMPDIR/$BUNDLEFILENAME)"
-              return $CODEOTHERERR
-            fi
-          else
-            log_msg_error "File $ORIGPDBFILENAME does not exist in archive $TMPDIR/$BUNDLEFILENAME"
-            return $CODEOTHERERR
-          fi
-        fi
-      fi
-  fi
+  PDBFILENAME="target_${TARGET_SYSTEM_NAME}.pdb" 
+  wget -q -O "$TMPDIR/$PDBFILENAME" "https://files.rcsb.org/download/$PDBSTR.pdb" 2>/dev/null 
+  if [ $? -ne 0 ]; then 
+    rm "$TMPDIR/$PDBFILENAME"; RESULT="NULL"; 
+    log_msg_error "Failed to download PDB file (https://files.rcsb.org/download/$PDBSTR.pdb)"; return $CODEOTHERERR;
+  else RESULT="$TMPDIR/$PDBFILENAME"; fi
 fi
 
-echo "$RESULT" "$COMMENT"
-          
+echo "$RESULT"
