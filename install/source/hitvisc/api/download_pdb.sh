c48f83fc106d8b698cc98e1222a81412a9f3540a
hitvisc <nnikitina.krc@gmail.com>
2025-01-29 11:56:47 +0300

    Update download_pdb.sh - add support for large PDBs

--- install/source/hitvisc/api/download_pdb.sh
+++ install/source/hitvisc/api/download_pdb.sh
@@ -21,22 +21,61 @@
 
 # HiTViSC: скрипт скачивания PDB-файла мишени, скачанного с сайта RCSB по заданному идентификатору.
 #
-# Reference for the entries without ready PDB files:
-# https://www.rcsb.org/docs/general-help/structures-without-legacy-pdb-format-files
 # Usage: download_pdb.sh $TMPDIR $PDBSTR $TARGET_SYSTEM_NAME
+#
+# Reference for the entries without ready PDB files: https://www.rcsb.org/docs/general-help/structures-without-legacy-pdb-format-files
 
 TMPDIR="$1"
 PDBSTR="$2"
 TARGET_SYSTEM_NAME="$3"
-RESULT="NULL"
+TMPPDBFILENAME="NULL"
+COMMENT="NULL"
 
 if [[ "$PDBSTR" =~ ^[0-9A-Za-z]{4}$ ]]; then
-  PDBFILENAME="target_${TARGET_SYSTEM_NAME}.pdb" 
-  wget -q -O "$TMPDIR/$PDBFILENAME" "https://files.rcsb.org/download/$PDBSTR.pdb" 2>/dev/null 
-  if [ $? -ne 0 ]; then 
-    rm "$TMPDIR/$PDBFILENAME"; RESULT="NULL"; 
-    log_msg_error "Failed to download PDB file (https://files.rcsb.org/download/$PDBSTR.pdb)"; return $CODEOTHERERR;
-  else RESULT="$TMPDIR/$PDBFILENAME"; fi
+  PDBFILENAME="target_${TARGET_SYSTEM_NAME}.pdb" # filename in HiTViSc system
+  PDBURL="https://files.rcsb.org/download/${PDBSTR}.pdb"
+  status_code_pdb=$(curl -Is "$PDBURL" | head -n 1 | awk '{print $2}')
+  if [[ "$status_code_pdb" == "200" ]]; then
+    # Legacy PDB format file exists and is accessible: download it
+    wget -q -O "$TMPDIR/$PDBFILENAME" "$PDBURL" > /dev/null
+    if [ $? -ne 0 ]; then
+      rm "$TMPDIR/$PDBFILENAME"; TMPPDBFILENAME="NULL";
+      log_msg_error "Failed to download PDB file from the RCSB PDB database (URL: $PDBURL)"
+      return $CODEOTHERERR
+    else
+      TMPPDBFILENAME="$TMPDIR/$PDBFILENAME"
+      COMMENT="Note: legacy PDB format file for PDB ID $PDBSTR has been retrieved from the RCSB PDB database (URL: $PDBURL)."
+    fi
+  else
+    # Legacy PDB format file not found: check the availability of PDB bundle in the PDB FTP archive
+    DIR=$(cut -c 2-3 <<<"$PDBSTR" | tr '[:upper:]' '[:lower:]')
+    PDBSTRlower=$(echo "$PDBSTR" | tr '[:upper:]' '[:lower:]')
+    BUNDLEFILENAME="${PDBSTRlower}-pdb-bundle.tar.gz"
+    BUNDLEURL=$(echo "https://files.wwpdb.org/pub/pdb/compatible/pdb_bundle/$DIR/$PDBSTRlower/$BUNDLEFILENAME")
+    status_code_bundle=$(curl -Is "$BUNDLEURL" | head -n 1 | awk '{print $2}')
+      if [[ "$status_code_bundle" == "200" ]]; then
+        wget -q -O "$TMPDIR/$BUNDLEFILENAME" "$BUNDLEURL" > /dev/null
+        if [ $? -ne 0 ]; then
+          rm "$TMPDIR/$BUNDLEFILENAME"; TMPPDBFILENAME="NULL";
+          log_msg_error "Failed to download PDB bundle (URL: $BUNDLEURL)"
+          return $CODEOTHERERR
+        else
+          ORIGPDBFILENAME="${PDBSTRlower}-pdb-bundle1.pdb"
+          if tar --list -f "$TMPDIR/$BUNDLEFILENAME" 2>/dev/null | grep -q "$ORIGPDBFILENAME"; then
+            if tar -xzvf "$TMPDIR/$BUNDLEFILENAME" --to-stdout "$ORIGPDBFILENAME" 2>/dev/null > "$TMPDIR/$PDBFILENAME"; then
+              TMPPDBFILENAME="$TMPDIR/$PDBFILENAME"
+              COMMENT="Note: legacy PDB format file for PDB ID $PDBSTR was not available for this structure. The best effort/minimal PDB format file has been retrieved from URL: $BUNDLEURL (file $ORIGPDBFILENAME). See more information at https://www.rcsb.org/docs/general-help/structures-without-legacy-pdb-format-files."
+            else
+              log_msg_error "Error extracting PDB bundle ($ORIGPDBFILENAME from $TMPDIR/$BUNDLEFILENAME)"
+              return $CODEOTHERERR
+            fi
+          else
+            log_msg_error "File $ORIGPDBFILENAME does not exist in archive $TMPDIR/$BUNDLEFILENAME"
+            return $CODEOTHERERR
+          fi
+        fi
+      fi
+  fi
 fi
 
-echo "$RESULT"
+echo "$TMPPDBFILENAME" "\"$COMMENT\""
