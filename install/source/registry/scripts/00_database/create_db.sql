6ab760fef331989264b3cf7e28c8ba161d960661
hitvisc <nnikitina.krc@gmail.com>
2025-02-12 17:21:18 +0300

    Update create_db.sql

--- install/source/registry/scripts/00_database/create_db.sql
+++ install/source/registry/scripts/00_database/create_db.sql
@@ -3,7 +3,7 @@ GRANT pg_read_server_files TO hitvisc_owner;
 
 CREATE DATABASE hitvisc WITH OWNER hitvisc_owner;
 
-CREATE USER hitviscadm WITH PASSWORD 'h1vi_124#sc8tv';
+CREATE USER hitviscadm WITH SUPERUSER PASSWORD 'h1vi_124#sc8tv';
 GRANT hitvisc_owner TO hitviscadm;
 ALTER USER hitviscadm PASSWORD 'h1vi_124#sc8tv';
 ALTER USER hitviscadm SET ROLE hitvisc_owner;
