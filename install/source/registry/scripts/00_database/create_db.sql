814c4c3be7af52ad6b2ecf79d30f3c49058af742
hitvisc <nnikitina.krc@gmail.com>
2025-02-13 14:50:28 +0300

    Update create_db.sql - add option SUPERUSER

--- install/source/registry/scripts/00_database/create_db.sql
+++ install/source/registry/scripts/00_database/create_db.sql
@@ -5,7 +5,5 @@ CREATE DATABASE hitvisc WITH OWNER hitvisc_owner;
 
 CREATE USER hitviscadm WITH SUPERUSER PASSWORD 'h1vi_124#sc8tv';
 GRANT hitvisc_owner TO hitviscadm;
-ALTER USER hitviscadm PASSWORD 'h1vi_124#sc8tv';
+ALTER USER hitviscadm WITH SUPERUSER PASSWORD 'h1vi_124#sc8tv';
 ALTER USER hitviscadm SET ROLE hitvisc_owner;
-
-
