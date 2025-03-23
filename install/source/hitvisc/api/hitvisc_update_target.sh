0d332024d0b24d2c27380cf7980d2fd04e5eb696
Natalia <nnikitina.krc@gmail.com>
2025-03-23 14:45:12 +0300

    Create hitvisc_update_target.sh

---
+++ install/source/hitvisc/api/hitvisc_update_target.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#    Copyright (C) 2024 HiTViSc Contributors
+#    Contact e-mail: hitvisc@yandex.ru
+#
+#    GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Usage:
+USAGE_TEMPLATE="hitvisc_update_target.sh FRONT_ENTITY_ID NAME USAGE_TYPE DESCRIPTION"
+
+# Get the number of arguments from template:
+NARGS="$(awk '{print NF-1}' <<< "$USAGE_TEMPLATE")"
+if [ "$#" -ne $NARGS ]; then log_msg_error "Wrong number of arguments ($#)"; exit $CODEARGERR; fi
+
+DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+source "$DIR/../api/hitvisc_api_init.sh"
+
+source "$DIR/update_target.sh" "$@"
+EXITCODE=$?
+if [ $EXITCODE -ne $CODESUCCESS ]; then
+  let ERRCODE=$EXITCODE-256; echo $ERRCODE; exit $EXITCODE; fi
+
+if [ -z $TARGET_ID ] || [ $TARGET_ID -le 0 ]; then echo $CODEOTHERERR; exit $CODEOTHERERR; fi
