5cdce0cde265d625f623660f975f9644aaad21d0
Natalia <nnikitina.krc@gmail.com>
2025-03-23 13:58:32 +0300

    Create archive_target.sh

---
+++ install/source/hitvisc/api/archive_target.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+#    Copyright (C) 2024 HiTViSc Contributors
+#    Contact e-mail: hitvisc@yandex.ru
+#
+#    GNU GENERAL PUBLIC LICENSE
+#    Version 3, 29 June 2007
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Arguments: FRONT_TARGET_ID
+
+FRONT_TARGET_ID="$1"; if [[ ! $FRONT_TARGET_ID -gt 0 ]]; then
+  log_msg_error "invalid value of FRONT_TARGET_ID ($FRONT_TARGET_ID)"; return $CODEARGERR; fi
+
+TARGET_ID=$(echo "SELECT back_entity_id FROM registry.entity_mapping WHERE front_entity_id = $FRONT_TARGET_ID AND entity_type = 'T'" | psql --dbname=hitvisc -qtA)
+
+if [[ ! $TARGET_ID -gt 0 ]]; then
+  log_msg_error "invalid value of TARGET_ID ($TARGET_ID)"; return $CODEARGERR; fi
+
+PSQL_STATUS=$(echo "UPDATE registry.target SET state = 'A' WHERE id = $TARGET_ID" | psql --dbname=hitvisc -qtA)
+
+if [[ ! "$PSQL_STATUS" -gt 0 ]]; then
+  log_msg_error "unable to archive the target ($TARGET_ID)"
+  exit 1
+fi
