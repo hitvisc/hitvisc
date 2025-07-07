3ed7bbd9062adc441420887696008f382545ec0e
Natalia Kukushkina <nnikitina.krc@gmail.com>
2025-07-07 17:01:17 +0300

    Update shell.service.ts

--- frontend/backend/src/src/shell/services/shell.service.ts
+++ frontend/backend/src/src/shell/services/shell.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { execFile, ExecFileOptions } from 'node:child_process';
+import { exec, ExecOptions } from 'node:child_process';
 import { promisify } from 'node:util';
 import { ConfigService } from '@nestjs/config';
 
-const asyncExecFile = promisify(execFile);
+const asyncExec = promisify(exec);
 
 @Injectable()
 export class ShellService {
   constructor(private readonly configService: ConfigService) {}
 
-  async runShellCommand(command: string, args: string[]) {
-    const options: ExecFileOptions = {};
+  async runShellCommand(command: string) {
+    const options: ExecOptions = {};
     if (this.configService.get<string>('SHELL_UID')) {
       options.uid = +this.configService.get<number>('SHELL_UID');
     }
-    return await asyncExecFile(command, args, options);
+    return await asyncExec(command, options);
   }
 }
